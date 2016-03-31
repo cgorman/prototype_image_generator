8667fab55b8c89118ae106118626f3ef3b4cf645
Chris Gorman <chris@cgorman.net>
2016-04-01 11:22:05 +1300

    Added argument parsing Completed the argument parsing code Added arguments file for dataset 1 Added directory for storing datasets

--- generate_dataset.py
+++ generate_dataset.py
@@ -16,6 +16,18 @@ user to make things a bit easier.
 """
 
 
+def percentage_float(x):
+    """
+    Converts a string to a float and requires it to be between 0 and 1 inclusive
+    :param x: Something that can be cast to float
+    :return: The converted float
+    """
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+    return x
+
+
 def create_shape(shape, color, texture=None):
     """
     Creates an instance of a shape with the given properties
@@ -76,14 +88,19 @@ def run():
     parser = argparse.ArgumentParser(prog="generate_dataset.py",
                                      description="Script to generate prototype images for research on category "
                                                  "development. Generates images which conform to certain statistics "
-                                                 "in order to test hypotheses about prototype theory.")
+                                                 "in order to test hypotheses about prototype theory. "
+                                                 "NOTE: Pass an arguments file prefixed with @ as an arg "
+                                                 "to include the arguments listed in that file. Each argument and value"
+                                                 " should be on separate lines.",
+                                     epilog="(Again, you may want to use a file instead)",
+                                     fromfile_prefix_chars="@")
     parser.add_argument("--output-directory",
                         help="The directory to save the dataset to.")
     parser.add_argument("--dataset-name",
                         help="Colloquial name for this dataset. Will be the name of the final output directory.")
     parser.add_argument("--random-stats",
-                        hellp="Script will generate sensible statistics for shapes at random. All shapes will be used."
-                              " If this argument is set, the script will ignore any manual statistics that follow!",
+                        help="Script will generate sensible statistics for shapes at random. All shapes will be used."
+                             " If this argument is set, the script will ignore any manual statistics that follow!",
                         action="store_true")
 
     square_group = parser.add_argument_group("Square Statistics")
@@ -91,19 +108,59 @@ def run():
                               help="The color of the prototypical square",
                               choices=color_choices)
     square_group.add_argument("--square-percent-color",
-                              help="The percent of squares that are the previous color. The rest will be random",
-                              type=float)
-    square_group.add_argument_group("--square-texture",
-                                    help="The texture of the prototypical square",
-                                    choices=texture_choices)
+                              help="The percent of squares that are the previous color. The rest will be random. "
+                                   "Value must be in range [0.0, 1.0]",
+                              type=percentage_float)
+    square_group.add_argument("--square-texture",
+                              help="The texture of the prototypical square",
+                              choices=texture_choices)
     square_group.add_argument("--square-percent-texture",
-                              help="The percent of squares that are the previous texture. The rest will be random",
-                              type=float)
+                              help="The percent of squares that are the previous texture. The rest will be random. "
+                                   "Value must be in range [0.0, 1.0]",
+                              type=percentage_float)
     square_group.add_argument("--square-number",
                               help="The number of squares we should generate as output",
                               type=int)
 
     circle_group = parser.add_argument_group("Circle Statistics")
+    circle_group.add_argument("--circle-color",
+                              help="The color of the prototypical circle",
+                              choices=color_choices)
+    circle_group.add_argument("--circle-percent-color",
+                              help="The percent of circles that are the previous color. The rest will be random. "
+                                   "Value must be in range [0.0, 1.0]",
+                              type=percentage_float)
+    circle_group.add_argument("--circle-texture",
+                              help="The texture of the prototypical circle",
+                              choices=texture_choices)
+    circle_group.add_argument("--circle-percent-texture",
+                              help="The percent of circles that are the previous texture. The rest will be random. "
+                                   "Value must be in range [0.0, 1.0]",
+                              type=percentage_float)
+    circle_group.add_argument("--circle-number",
+                              help="The number of circles we should generate as output",
+                              type=int)
+
+    triangle_group = parser.add_argument_group("Triangle Statistics")
+    triangle_group.add_argument("--triangle-color",
+                                help="The color of the prototypical triangle",
+                                choices=color_choices)
+    triangle_group.add_argument("--triangle-percent-color",
+                                help="The percent of triangles that are the previous color. The rest will be random. "
+                                     "Value must be in range [0.0, 1.0]",
+                                type=percentage_float)
+    triangle_group.add_argument("--triangle-texture",
+                                help="The texture of the prototypical triangle",
+                                choices=texture_choices)
+    triangle_group.add_argument("--triangle-percent-texture",
+                                help="The percent of triangles that are the previous texture. The rest will be random. "
+                                     "Value must be in range [0.0, 1.0]",
+                                type=percentage_float)
+    triangle_group.add_argument("--triangle-number",
+                                help="The number of triangles we should generate as output",
+                                type=int)
+
+    args = parser.parse_args()
 
     pass
 
